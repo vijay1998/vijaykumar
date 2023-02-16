6310e3008c52d632b7156062ed1f53b23c2ca39d
vijay1998 <karedlavijaykumar777@gmail.com>
2023-02-16 19:29:39 +0530

    Assignment docker hub

---
+++ raj/src/main/java/com/example/raj/RajApplication.java
@@ -0,0 +1,13 @@
+package com.example.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajApplication.class, args);
+	}
+
+}
---
+++ raj/src/test/java/com/example/raj/RajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
